4e9b9a4671003612c1e438101cbbf6803afde476
Bill Janssen <janssen@parc.com>
2018-07-29 13:15:31 -0700

    remove separate removehashes script

--- ripping/removehashes.py
+++
@@ -1,6 +0,0 @@
-import sys, os, re
-
-for dirpath, dirnames, filenames in os.walk(sys.argv[1]):
-    for filename in filenames:
-        if '#' in filename:
-            os.rename(os.path.join(dirpath, filename), os.path.join(dirpath, re.sub('#', 'No. ', filename)))
